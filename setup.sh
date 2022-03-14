7bb26d5ed0c456b22a94841b556dacbf4b699179
Brian Petersen <skiwalk3r@gmail.com>
2022-03-14 12:37:11 -0600

    Removes tmux plugins

--- setup.sh
+++ setup.sh
@@ -3,7 +3,6 @@
 git submodule update --init --recursive
 
 mkdir -p $HOME/.config
-mkdir -p $HOME/.tmux/plugins
 
 ln -sT $HOME/.dotfiles/gdbinit $HOME/.gdbinit
 ln -sT $HOME/.dotfiles/gitconfig $HOME/.gitconfig
@@ -11,4 +10,3 @@ ln -sT $HOME/.dotfiles/tmux.conf $HOME/.tmux.conf
 ln -sT $HOME/.dotfiles/zshrc $HOME/.zshrc
 ln -sT $HOME/.dotfiles/nvim $HOME/.config/nvim
 ln -sT $HOME/.dotfiles/oh-my-zsh $HOME/.oh-my-zsh
-ln -sT $HOME/.dotfiles/tpm $HOME/.tmux/plugins/tpm
