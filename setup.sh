9f293191ed940d981bd7df11122228b8ede97d24
Brian Petersen <skiwalk3r@gmail.com>
2024-02-08 20:52:12 -0700

    Dotfiles update

--- setup.sh
+++ setup.sh
@@ -6,8 +6,9 @@ git submodule update --init --recursive
 
 mkdir -p $HOME/.config
 
-ln -sT $HOME/.dotfiles/kitty $HOME/.config/kitty
+ln -sT $HOME/.dotfiles/gdb-dashboard/.gdbinit $HOME/.gdbinit
 ln -sT $HOME/.dotfiles/gitconfig $HOME/.gitconfig
+ln -sT $HOME/.dotfiles/kitty $HOME/.config/kitty
 ln -sT $HOME/.dotfiles/nvim $HOME/.config/nvim
 ln -sT $HOME/.dotfiles/oh-my-zsh $HOME/.oh-my-zsh
 ln -sT $HOME/.dotfiles/tmux.conf $HOME/.tmux.conf
