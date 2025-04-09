dcca2df01159e2ef88eb7fcb9f5e23ec1abe84c9
Brian Petersen <bp@jumpapp.com>
2025-04-09 11:28:57 -0600

    MAS CAMBIOS

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -6,8 +6,14 @@ vim.diagnostic.config({
   virtual_text = true,
 })
 
+-- Options: "trace", "debug", "info", "warn", "error", "off"
+vim.lsp.set_log_level("warn")
+
 --
--- Set up and register configs
+-- Set up and register configs.
+--
+-- See https://github.com/neovim/nvim-lspconfig/tree/master/lua/lspconfig/configs
+-- for a starting point if needed.
 --
 local configs = {
   elixirls = {
@@ -26,6 +32,19 @@ local configs = {
     cmd = { 'rust-analyzer' },
     filetypes = { 'rust' },
     root_markers = { 'Cargo.toml' },
+  },
+
+  tsserver = {
+    cmd = { 'typescript-language-server', '--stdio' },
+    filetypes = {
+      'javascript',
+      'javascriptreact',
+      'javascript.jsx',
+      'typescript',
+      'typescriptreact',
+      'typescript.tsx',
+    },
+    root_markers = { 'tsconfig.json', 'jsconfig.json', 'package.json' },
   },
 }
 
@@ -68,15 +87,21 @@ end, {})
 -- Set up custom keymaps
 vim.api.nvim_create_autocmd('LspAttach', {
   callback = function(args)
-    local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-
     vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, { buffer = true, desc = 'Go to definition' })
-    vim.keymap.set('n', 'gr', function() require('telescope.builtin').lsp_references() end, { buffer = true, desc = 'List references' })
-    vim.keymap.set('n', 'gl', function() vim.diagnostic.open_float() end, { buffer = true, desc = 'Open diagnostic window' })
+
+    vim.keymap.set('n', 'gr', function() require('telescope.builtin').lsp_references() end,
+      { buffer = true, desc = 'List references' })
+
+    vim.keymap.set('n', 'gl', function() vim.diagnostic.open_float() end,
+      { buffer = true, desc = 'Open diagnostic window' })
+
     vim.keymap.set('n', '<leader>fm', function() vim.lsp.buf.format() end, { buffer = true, desc = 'Format file' })
 
-    vim.keymap.set('n', 'gO', function() require('telescope.builtin').lsp_document_symbols() end, { buffer = true, desc = 'Document symbols' })
-    vim.keymap.set('n', 'gW', function() require('telescope.builtin').lsp_workspace_symbols() end, { buffer = true, desc = 'Workspace symbols' })
+    vim.keymap.set('n', 'gO', function() require('telescope.builtin').lsp_document_symbols() end,
+      { buffer = true, desc = 'Document symbols' })
+
+    vim.keymap.set('n', 'gW', function() require('telescope.builtin').lsp_workspace_symbols() end,
+      { buffer = true, desc = 'Workspace symbols' })
   end
 })
 
