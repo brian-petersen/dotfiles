ebd9ca78d9fc0026e934e78069b5e33c926aae33
Brian Petersen <bp@jumpapp.com>
2026-03-23 11:12:42 -0600

    get dem tests flying

--- nvim/lua/plugins/test.lua
+++ nvim/lua/plugins/test.lua
@@ -1,42 +1,54 @@
+local function cmd_fallback(cmd)
+  vim.cmd("split | term " .. cmd)
+end
+
+local function run_in_smart_tmux(cmd)
+  if not os.getenv("TMUX_PANE") then
+    cmd_fallback(cmd)
+    return
+  end
+
+  local query = "tmux list-panes -F '#{pane_id} #{pane_at_right} #{pane_current_command}'"
+  local handle = io.popen(query)
+  local result = handle:read("*a")
+  handle:close()
+
+  local target_pane = nil
+  local shell_list = { ["zsh"] = true, ["bash"] = true, ["sh"] = true, ["fish"] = true }
+
+  for line in result:gmatch("[^\r\n]+") do
+    local id, is_right, current_proc = line:match("(%%%d+) (%d) ([%w%-]+)")
+    if is_right == "1" and shell_list[current_proc] then
+      target_pane = id
+      break
+    end
+  end
+
+  if target_pane then
+    -- Clear the line (C-u) and run the command (C-m is Enter)
+    local tmux_cmd = string.format("tmux send-keys -t %s C-u '%s' C-m", target_pane, cmd)
+    os.execute(tmux_cmd)
+  else
+    cmd_fallback(cmd)
+  end
+end
+
 return {
   {
-    'nvim-neotest/neotest',
+    'vim-test/vim-test',
     config = function()
-      require('neotest').setup({
-        adapters = {
-          require('neotest-elixir')({
-            -- Modified filter dir to allow running test files under 'lib/'.
-            -- Taken from https://github.com/jfpedroza/neotest-elixir/blob/a242aebeaa6997c1c149138ff77f6cacbe33b6fc/lua/neotest-elixir/init.lua#L43
-            filter_dir = function(_name, rel_path, _root)
-              return rel_path == 'test'
-                  or vim.startswith(rel_path, 'test/')
-                  or rel_path == 'lib'
-                  or vim.startswith(rel_path, 'lib/')
-                  or rel_path == 'apps'
-                  or rel_path:match('^apps/[^/]+$')
-                  or rel_path:match('^apps/[^/]+/test')
-            end,
-          }),
-        },
-      })
+      vim.g["test#custom_strategies"] = {
+        smart_tmux = function(cmd)
+          run_in_smart_tmux(cmd)
+        end
+      }
+
+      vim.g["test#strategy"] = "smart_tmux"
     end,
     keys = {
-      { '<leader>tn', function() require('neotest').run.run() end, desc = '[T]est [n]earest' },
-      { '<leader>tb', function() require('neotest').run.run(vim.fn.expand('%')) end, desc = '[T]est [b]uffer' },
-      { '<leader>tl', function() require('neotest').run.run_last() end, desc = '[T]est [l]ast' },
-
-      { '<leader>ts', function() require('neotest').summary.toggle() end, desc = '[T]est [s]ummary toggle' },
-      { '<leader>to', function() require('neotest').output_panel.toggle() end, desc = '[T]est [o]utput panel toggle' },
-      { '<leader>tc', function() require('neotest').output_panel.clear() end, desc = '[T]est output panel [c]lear' },
+      { '<leader>tn', ':TestNearest<CR>', desc = '[T]est [n]earest' },
+      { '<leader>tb', ':TestFile<CR>',    desc = '[T]est [b]uffer' },
+      { '<leader>tl', ':TestLast<CR>',    desc = '[T]est [l]ast' },
     },
-    dependencies = {
-      -- General neotest deps
-      'nvim-neotest/nvim-nio',
-      'nvim-lua/plenary.nvim',
-      'antoinemadec/FixCursorHold.nvim',
-
-      -- Language specific adapters
-      'jfpedroza/neotest-elixir'
-    },
-  },
+  }
 }
